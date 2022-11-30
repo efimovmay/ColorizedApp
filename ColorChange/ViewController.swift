4c66266f06384b79a143e6990734433049569fcb
Aleksey Efimov <smaylik121@gmail.com>
2022-12-01 00:09:25 +0300

    work with constraint cable

--- ColorChange/ViewController.swift
+++ ColorChange/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 10
+        
     }
 
 
